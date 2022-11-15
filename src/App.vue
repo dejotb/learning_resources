0769bb12a5a41084e44a13776e6bc2e9f924e2e4
dejotb <dejotb@gmail.com>
2022-11-15 17:37:23 +0100

    added header and buttons styling

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
+  <the-header title="RememberMe"></the-header>
   <stored-resources :resources="storedResources"></stored-resources>
 </template>
 
 <script>
 import storedResources from './components/learning-resources/StoredResources.vue';
+import TheHeader from './components/layouts/TheHeader.vue';
 
 export default {
-  components: { storedResources },
+  components: { storedResources, TheHeader },
 
   data() {
     return {
